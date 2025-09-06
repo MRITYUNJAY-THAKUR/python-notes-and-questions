6232a1fa1ae4df35ad09e557e44656fffecda300
Mrityunjay Thakur <thakurmrityunjay452@gmail.com>
2025-09-06 19:11:43 +0530

    Add files via upload

---
+++ first class.py
@@ -0,0 +1,60 @@
+print("MRITYUNJAY THAKUR")
+print("my age is 20")
+print("i am ok.","how are you")
+name= "MRITYUNJAY THAKUR"
+age= 20 
+price = 25.99
+print(name)
+print(age)
+print(price)
+print("price")
+print("my name is:", name)
+name2=name
+print(name2)
+print(type(name))
+print(type(age))
+print(type(price))
+old=False
+print(type(old))
+a = 7
+b =8
+sum =a+b
+
+print(sum)
+print("My name is",name)
+print(type(age))
+diff= a - b
+print(diff)
+ #arithmetic operators
+ #we can also write print(a + b) or (a - b)
+print(a - b)
+print(a + b)
+print(a / b )
+print(a * b)
+print(b % a) #remainder
+print(a ** b)#a^b is given
+#relational operartor
+x = 6
+y = 9
+print(x == y) # mean is x equal to y?
+print(x != y) 
+#same for <,>,<=,>= as well.
+num= 68
+num+= 7
+print(num)# short form to write num=num + 7
+#logical operators
+print(not True)
+print(not False)
+print(not (x > y))# mean opposite of x > y 
+print((x == y) or (x < y)) #or operator
+a = 2
+b = 3.49
+print(a + b) #here python will automatically convert 2 to a boolean that is 2.00
+#type cast
+a =  int("2") #just write before the type you want to convert
+b = 3.49
+print(a + b)
+b = str(b)
+print(type(b))
+#input in python
+input("enter your name:")
